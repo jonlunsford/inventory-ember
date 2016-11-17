6f750a725498392292f5926848451741a780addf
Jon Lunsford <jon@capturethecastle.net>
2016-11-17 06:09:20 -0800

    Layout updates to logged in app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, inject } = Ember;
+
+export default Route.extend({
+  session: inject.service(),
+
+  beforeModel() {
+    if(this.get('session').get('isAuthenticated')) {
+      this.transitionTo('app.index');
+    }
+  }
 });
